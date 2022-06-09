89ffceb78d8b76db1c9834ab7e3c1e6e0613c43b
Константин Васильев <89670253660@mail.ru>
2022-06-09 22:45:24 +0300

    Add location to forms

--- memories/memo/forms.py
+++ memories/memo/forms.py
@@ -6,4 +6,4 @@ from .models import InMemories
 class InMemoriesForm(forms.ModelForm):
     class Meta:
         model = InMemories
-        fields = ('title', 'comment')
+        fields = ('title', 'comment', 'location')
