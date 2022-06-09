627cba34b8c74f0eab3c13cb82dc2165ed9893ac
Константин Васильев <89670253660@mail.ru>
2022-06-09 21:32:07 +0300

    Add Meta to admin

--- memories/memo/admin.py
+++ memories/memo/admin.py
@@ -5,4 +5,7 @@ from .models import InMemories
 class InMemoriesAdmin(admin.ModelAdmin):
     list_display = ('title', 'comment', 'location')
 
+    class Meta:
+        verbose_name_plural = 'In memories'
+
 admin.site.register(InMemories, InMemoriesAdmin)
