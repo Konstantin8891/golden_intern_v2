f62fbdb916f104f5c1ceac96d1a2be31efda17be
Константин Васильев <89670253660@mail.ru>
2022-06-10 21:25:12 +0300

    Add marker on map from db

--- memories/memo/views.py
+++ memories/memo/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import FileResponse
 from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.gis.db.models.functions import AsGeoJSON
 
 from .forms import InMemoriesForm
 from .models import InMemories
