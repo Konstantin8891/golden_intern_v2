832afe7d031f09819cc834c5d501b2a5de313ffb
Константин Васильев <89670253660@mail.ru>
2022-06-11 19:52:37 +0300

    Add help text to models

--- memories/memo/models.py
+++ memories/memo/models.py
@@ -6,12 +6,19 @@ User = get_user_model()
 
 
 class InMemories(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
     title = models.CharField(
         verbose_name='Заголовок',
+        help_text='Введите название воспоминания'
         max_length=255
     )
-    comment = models.TextField(verbose_name='Воспоминание')
+    comment = models.TextField(
+        verbose_name='Воспоминание',
+        help_text='Введите текст воспоминания'
+    )
     location = models.PointField(srid=4326)
 
     class Meta:
